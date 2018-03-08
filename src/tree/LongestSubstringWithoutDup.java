6bd1460849725f7204add2c125c9860e5fbdc3e8
Changeset <695700827@qq.com>
2018-03-08 22:04:31 +0800

    update dynamic programming code

---
+++ src/tree/LongestSubstringWithoutDup.java
@@ -0,0 +1,14 @@
+package tree;
+
+/**
+ * @ Author: Xuelong Liao
+ * @ Description:
+ * @ Date: created in 21:55 2018/3/8
+ * @ ModifiedBy:
+ */
+public class LongestSubstringWithoutDup {
+    public static int longestSubstringWithoutDuplication(String s) {
+        int longestCur = 0, longestAll = 0;
+
+    }
+}

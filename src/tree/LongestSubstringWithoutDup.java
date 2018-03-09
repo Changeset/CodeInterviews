9dd2bb7bba8f1eba155e5845a1b1cc59b527cb75
Changeset <695700827@qq.com>
2018-03-09 09:12:24 +0800

    update dynamic programming code

--- src/tree/LongestSubstringWithoutDup.java
+++ src/tree/LongestSubstringWithoutDup.java
@@ -8,7 +8,27 @@ package tree;
  */
 public class LongestSubstringWithoutDup {
     public static int longestSubstringWithoutDuplication(String s) {
-        int longestCur = 0, longestAll = 0;
+        int curLength = 0, maxLength = 0;
+        int[] prevIndex = new int[26];
+        int n = s.length();
+        for (int i = 0; i < prevIndex.length; i++) {
+            prevIndex[i] = -1;
+        }
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (i - prevIndex[c - 'a'] > curLength || prevIndex[c - 'a'] < 0)
+                curLength++;
+            else {
+                curLength = i - prevIndex[c - 'a'];
+            }
+            prevIndex[c-'a'] = i;
+            maxLength = Math.max(curLength, maxLength);
+        }
+        return maxLength;
+    }
 
+    public static void main(String[] args) {
+        String s = "abcardf";
+        System.out.println(LongestSubstringWithoutDup.longestSubstringWithoutDuplication(s));
     }
 }

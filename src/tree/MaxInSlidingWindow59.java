7383b662a6e499bb83bb21714d5614bbf336a998
Changeset <695700827@qq.com>
2018-03-11 21:05:27 +0800

    update balanced Binary Tree for java. add Max In sliding window problems.

---
+++ src/tree/MaxInSlidingWindow59.java
@@ -0,0 +1,13 @@
+package tree;
+
+/**
+ * @ Author: Xuelong Liao
+ * @ Description:
+ * @ Date: created in 19:55 2018/3/11
+ * @ ModifiedBy:
+ */
+public class MaxInSlidingWindow59 {
+    public int[] maxInSlidingWindow(int[] nums, int k) {
+
+    }
+}
---
+++ src/tree/TwoNumbersWithSum57.java
@@ -0,0 +1,11 @@
+package tree;
+
+/**
+ * @ Author: Xuelong Liao
+ * @ Description:
+ * @ Date: created in 17:32 2018/3/11
+ * @ ModifiedBy:
+ */
+public class TwoNumbersWithSum57 {
+
+}
